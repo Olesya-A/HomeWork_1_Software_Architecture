5e4b8ffe2c5817a14d8de5c45cc3b8bf7e83f4ed
Olesya <lesusha1@yandex.ru>
2024-01-31 15:48:18 +0300

    hw_1 added

---
+++ src/main/java/ModelElements/Flash.java
@@ -0,0 +1,17 @@
+package ModelElements;
+
+import Stuff.Angle3D;
+import Stuff.Point3D;
+import Stuff.Color;
+import Stuff.Float;
+
+public class Flash {
+    public Point3D Location;
+    public Angle3D Angle;
+    public Color Color;
+    public Float Power;
+
+    public void Rotate(Angle3D angle) {}
+
+    public void Move(Point3D location) {}
+}
